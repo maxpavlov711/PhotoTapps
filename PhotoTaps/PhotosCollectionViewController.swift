d758314297e558d9a9b0eb5bb06c766bb7bf39a2
Max Pavlov <max.pavlov711@gmail.com>
2022-01-21 12:29:56 +0300

    Flow Layout Advanced Setup

--- PhotoTaps/PhotosCollectionViewController.swift
+++ PhotoTaps/PhotosCollectionViewController.swift
@@ -8,11 +8,21 @@
 import UIKit
 
 class PhotosCollectionViewController: UICollectionViewController {
+    
+    let itemsPerRows: CGFloat = 2 // объекты на ряд
+    let sectionInserts = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
+//        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+//        layout.itemSize = CGSize(width: 70, height: 30)
+//        layout.sectionInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+//        layout.minimumLineSpacing = 1
+//        layout.minimumInteritemSpacing = 1
+//        layout.scrollDirection = .vertical
+//
+//        collectionView.showsVerticalScrollIndicator = false
     }
 
 
@@ -32,7 +42,7 @@ class PhotosCollectionViewController: UICollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! PhotoCell
         
-        cell.backgroundColor = .blue
+        cell.backgroundColor = .black
     
         return cell
     }
@@ -41,22 +51,21 @@ class PhotosCollectionViewController: UICollectionViewController {
 extension PhotosCollectionViewController: UICollectionViewDelegateFlowLayout {
 //    Размер ячейки
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let itemsPerRows: CGFloat = 2 // объекты на ряд
-        let paddingWith = 20 * (itemsPerRows + 1) // Сколько отступов на ряду будет
+        let paddingWith = sectionInserts.left * (itemsPerRows + 1) // Сколько отступов на ряду будет
         let availableWidth = collectionView.frame.width - paddingWith // Какая ширина доступна для ячеек
         let widthPerItem = availableWidth / itemsPerRows // точно доступна ширина для 1 объекта
         return CGSize(width: widthPerItem, height: widthPerItem)
     }
 //    Создание отступов (границы) ячеек
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        return sectionInserts
     }
 //    Растояние по линиям
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 20
+        return sectionInserts.left
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 20
+        return sectionInserts.left
     }
 }
